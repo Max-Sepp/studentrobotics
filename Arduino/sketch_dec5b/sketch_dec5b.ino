ba824d4c69696662dc629a8ef54b38b28c8cb9f0
Alex <alex_ha_192@protonmail.com>
2023-12-05 17:00:09 +0000

    Added some basic arduino code

--- Arduino/sketch_dec5b/sketch_dec5b.ino
+++ Arduino/sketch_dec5b/sketch_dec5b.ino
@@ -1,7 +1,7 @@
-#define FL
-#define FR
-#define BL
-#define BR
+#define FL // Pin for front left motor
+#define FR // Pin for front right motor
+#define BL // etc
+#define BR // etc
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,4 +11,10 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void writePins(int[] pins, int[] values) {
+  for (i = 0; i < sizeof(pins)/sizeof(pins[0]); i++) {
+    digitalWrite(pins[i], values[i])
+  }
 }
