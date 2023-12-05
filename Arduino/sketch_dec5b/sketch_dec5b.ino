e4549ad43358a2f760465fb088ce57f816b5c55d
Alex <alex_ha_192@protonmail.com>
2023-12-05 16:53:42 +0000

    Added arduino sketch

---
+++ Arduino/sketch_dec5b/sketch_dec5b.ino
@@ -0,0 +1,14 @@
+#define FL
+#define FR
+#define BL
+#define BR
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
