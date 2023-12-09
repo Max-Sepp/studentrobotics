b4b39cbb8e070d25679d11dd1aeac445e356ca24
Alex <alex_ha_192@protonmail.com>
2023-12-09 15:44:24 +0000

    fixed horizontal_angle syntax

--- Challenges/vision.py
+++ Challenges/vision.py
@@ -12,44 +12,37 @@ def Vision_Challenge_Angle():
             if marker.position.distance > 1:
                 robot.camera.save(robot.usbkey / "initial-view.jpg")
 
-            if marker.horizontal_angle <= -0.2618:
+            if marker.position.horizontal_angle <= -0.2618:
                 robot.kch.leds[LED_A].colour = Colour.YELLOW
             else:
                 robot.kch.leds[LED_A].colour = Colour.OFF
 
-            if -0.2618 < marker.horizontal_angle < 0.2618:
+            if -0.2618 < marker.position.horizontal_angle < 0.2618:
                 robot.kch.leds[LED_B].colour = Colour.YELLOW
             else:
                 robot.kch.leds[LED_B].colour = Colour.OFF
             
-            if marker.horizontal_angle >= 0.2618:
+            if marker.position.horizontal_angle >= 0.2618:
                 robot.kch.leds[LED_C].colour = Colour.YELLOW
             else:
                 robot.kch.leds[LED_C].colour = Colour.OFF
 
 def Vision_Challenge_Distance():
 
-    if marker.horizontal_angle > math.atan(0.2/distance):
+    if marker.position.horizontal_angle > math.atan(0.2/distance):
         robot.kch.leds[LED_C].colour = Colour.BLUE
     else:
         robot.kch.leds[LED_C].colour = Colour.OFF
 
-    if marker.horizontal_angle > -math.atan(0.2/distance) and marker.horizontal_angle < math.atan(0.2/distance):
+    if marker.position.horizontal_angle > -math.atan(0.2/distance) and marker.position.horizontal_angle < math.atan(0.2/distance):
         robot.kch.leds[LED_B].colour = Colour.YELLOW
     else:
         robot.kch.leds[LED_B].colour = Colour.OFF
 
-    if marker.horizontal_angle < -math.atan(0.2/distance):
+    if marker.position.horizontal_angle < -math.atan(0.2/distance):
         robot.kch.leds[LED_A].colour = Colour.BLUE
     else:
         robot.kch.leds[LED_A].colour = Colour.OFF
-        
 
-"""
-while True:
-    markers = robot.camera.see()
-    
-    Vision_Challenge_Angle(markers)
-"""
 
 Vision_Challenge_Angle()
